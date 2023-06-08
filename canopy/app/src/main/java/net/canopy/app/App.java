74e27c8ed4091e75bb754b8b1bb91f649fbdda5b
CrazyCraftix <43807375+CrazyCraftix@users.noreply.github.com>
2023-06-08 23:22:22 +0200

    rework main app

--- canopy/app/src/main/java/net/canopy/app/App.java
+++ canopy/app/src/main/java/net/canopy/app/App.java
@@ -2,14 +2,13 @@ package net.canopy.app;
 
 import net.canopy.app.api.IFilter;
 import net.canopy.app.api.Logger;
+import net.canopy.app.api.CanopyException;
 import net.canopy.app.api.FilterException;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.Set;
 import org.reflections.Reflections;
-import java.util.ArrayList;
-import java.util.HashSet;
 
 /**
  * Main class of Canopy. Contains the entry point of the application.
@@ -27,7 +26,7 @@ public class App {
      * @see IFilter
      * @see FilterCommand
      */
-    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException {
+    public static void main(String[] args) {
         if (args.length == 0) {
             logger.log("no filter commands specified");
             return;
@@ -39,72 +38,52 @@ public class App {
             filterCommands[i] = new FilterCommand(args[i]);
         }
 
-        var filterChain = loadFilters(filterCommands);
+        // get all classes implementing IFilter with classpath starting with net.canopy.filters
+        // this will be searched for matching classnames
+        Set<Class<? extends IFilter>> filterClasses = new Reflections("net.canopy.filters").getSubTypesOf(IFilter.class);
 
         try {
-            checkFilterOrder(filterChain);
-        } catch (FilterOrderException e) {
+            // create filter instances
+            for (FilterCommand command : filterCommands) {
+                command.createFilterInstance(filterClasses);
+            }
+
+            checkFilterOrder(filterCommands);
+
+            // run filter chain
+            JsonNode node = null;
+            for (FilterCommand command : filterCommands) {
+                try {
+                    node = command.applyFilter(node);
+                } catch (FilterException e) {
+                    logger.log("An error occured while running filter \"" + command.getFilterInstance().getClass().getName() + "\": " + e.getMessage() + ". The filter chain will be aborted.");
+                    break;
+                }
+            }
+
+        } catch (CanopyException e) {
             logger.log(e.getMessage());
             return;
         }
-
-        runFilterChain(filterChain, filterCommands);
     }
 
-    private static IFilter[] loadFilters(FilterCommand[] filterCommands) {
-        // get all classes implementing IFilter with classpath starting with net.canopy.filters
-        Set<Class<? extends IFilter>> filterClasses = new Reflections("net.canopy.filters").getSubTypesOf(IFilter.class);
-
-        IFilter[] filterChain = new IFilter[filterCommands.length];
-        for (int i = 0; i < filterCommands.length; i++) {
-                var arg = filterCommands[i];
-
-                Set<Class<? extends IFilter>> matchingFilterClasses = new HashSet<>();
-                for (Class<? extends IFilter> clazz : filterClasses) {
-                    if (clazz.getCanonicalName().endsWith(arg.filterClassName)) {
-                        matchingFilterClasses.add(clazz); // don't break to check for collisions
-                    }
-                }
-                Class<? extends IFilter> filterClass = matchingFilterClasses.iterator().next();
-
-                logger.log("Load filter: " + filterClass + " with arguments: " + arg.filterArgument);
-
-                try {
-                    filterChain[i] = (IFilter)filterClass.getDeclaredConstructor().newInstance();
-                } catch (Throwable e) { e.printStackTrace(); return null; }
-        }
-
-        return filterChain;
-    }
-
-    private static void checkFilterOrder(IFilter[] filterChain) throws FilterOrderException {
+    private static void checkFilterOrder(FilterCommand[] filterChain) throws CanopyException {
         // check if first node is IFilter.ILoadFilter
-        if (!(filterChain[0] instanceof IFilter.ILoadFilter)) {
-            throw new FilterOrderException("First filter in chain must be IFilter.ILoadFilter");
+        if (!(filterChain[0].getFilterInstance() instanceof IFilter.ILoadFilter)) {
+            throw new CanopyException("App.checkFilterOrder()", "First filter in chain must be IFilter.ILoadFilter");
         }
 
         // check if every Store-Filter is preceeded by a Store-Filter
         for (int i = 0; i < filterChain.length - 1; i++) {
-            if (!(filterChain[i] instanceof IFilter.IStoreFilter) && filterChain[i + 1] instanceof IFilter.ILoadFilter) {
-                throw new FilterOrderException("Load filters must be preceded by a store filter (except at the beginning)");
+            if (!(filterChain[i].getFilterInstance() instanceof IFilter.IStoreFilter) && filterChain[i + 1].getFilterInstance() instanceof IFilter.ILoadFilter) {
+                throw new CanopyException("App.checkFilterOrder()", "Load filters must be preceded by a store filter (except at the beginning)");
             }
         }
 
         // check if last node is IFilter.IStoreFilter
-        if (!(filterChain[filterChain.length - 1] instanceof IFilter.IStoreFilter)) {
-            throw new FilterOrderException("Last filter in chain must be IFilter.IStoreFilter");
+        if (!(filterChain[filterChain.length - 1].getFilterInstance() instanceof IFilter.IStoreFilter)) {
+            throw new CanopyException("App.checkFilterOrder()", "Last filter in chain must be IFilter.IStoreFilter");
         }
     }
 
-    private static void runFilterChain(IFilter[] filterChain, FilterCommand[] filterCommands) {
-        JsonNode node = null;
-        for (int i = 0; i < filterChain.length; i++) {
-            try {
-                node = filterChain[i].apply(node, filterCommands[i].filterArgument);
-            } catch (FilterException e) {
-                logger.log("An error occured while running filter \"" + filterChain[i].getClass().getName() + "\": " + e.getMessage() + ". The filter chain will be aborted.");
-                break;
-            }
-        }
-    }
 }
